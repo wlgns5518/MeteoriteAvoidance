a58217f54dfd5eace6aac2ba841abb3f4fe70869
jhwoo944 <tpfflaosej@gmail.com>
2024-01-25 10:44:53 +0900

    fix - GravityBody.cs 분리
    GravityBody.cs를 분리함
    Player_GravityBody.cs는 플레이어 전용
    GravityBody.cs는 베이스 코드로 참고하셔서 아이템, 운석 오브젝트에 적용하도록 해주세요.

--- Assets/Script/GravityBody.cs
+++ Assets/Script/GravityBody.cs
@@ -5,35 +5,21 @@ using UnityEngine;
 public class GravityBody : MonoBehaviour
 {
     private GravityAttractor attractor;
-    public float moveSpeed = 5f;
-    public float jumpForce = 10f;
-
-    private Rigidbody2D rb;
     private bool isGrounded;
 
     private void Start()
     {
         attractor = FindObjectOfType<GravityAttractor>();
-        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
-        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        attractor.Attract(GetComponent<Rigidbody2D>());
-        // 키 입력에 따라 오브젝트에 힘을 가하기
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector2 movement = new Vector2(horizontalInput, 0);
-        rb.AddForce(movement * moveSpeed);
+        attractor.Attract(GetComponent<Rigidbody2D>());     //지구의 중력에 계속 이끌리도록 적용하는 코드
 
-        // 점프
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            isGrounded = false;
-        }
+
+
     }
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D collision)    //지면에 닿았을 시 발동하는 코드
     {
         // 바닥에 닿으면 isGrounded를 true로 설정
         if (collision.gameObject.CompareTag("Ground"))
