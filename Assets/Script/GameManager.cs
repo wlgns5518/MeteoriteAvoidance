6198f46c7eb8f7b73fc6e57f12bddcfcbd3db46d
todreami <140941478+todreami@users.noreply.github.com>
2024-01-29 14:56:10 +0900

    fix/jaehyun/item/ edit spawn items
    edit spawn item's time(2f -> 5f)

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -109,7 +109,7 @@ public class GameManager : MonoBehaviour
         {
             if(currentSpawnCount == 0)
             {
-                yield return new WaitForSeconds(2f);
+                yield return new WaitForSeconds(5f);
 
                 if(currentWaveIndex % 10 == 0)
                 {
